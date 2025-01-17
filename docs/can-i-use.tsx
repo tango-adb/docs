ac6e1403ed05c86dd67834c7cdc9d741a08f1a09
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2025-01-17 13:16:00 +0800

    expand webcodecs page

---
+++ docs/can-i-use.tsx
@@ -0,0 +1,31 @@
+import Script from "./script";
+
+export default function CanIUse(props: { feature: string }) {
+    return (
+        <>
+            <p
+                className="ciu_embed"
+                data-feature={props.feature}
+                data-periods="future_1,current,past_1,past_2"
+                data-accessible-colours="true"
+            >
+                <picture>
+                    <source
+                        type="image/webp"
+                        srcSet={`https://caniuse.bitsofco.de/image/${props.feature}.webp`}
+                    />
+                    <source
+                        type="image/png"
+                        srcSet={`https://caniuse.bitsofco.de/image/${props.feature}.png`}
+                    />
+                    <img
+                        src={`https://caniuse.bitsofco.de/image/${props.feature}.jpg`}
+                        alt={`Data on support for the ${props.feature} feature across the major browsers from caniuse.com`}
+                    />
+                </picture>
+            </p>
+
+            <Script src="https://cdn.jsdelivr.net/gh/ireade/caniuse-embed@master/public/caniuse-embed.min.js" />
+        </>
+    );
+}
